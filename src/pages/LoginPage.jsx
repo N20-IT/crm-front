b37666fd5aa6ab7e5ca80e331b8b9828d1bae139
ruffaaw <rafal.p.ciupek@gmail.com>
2024-09-05 16:55:58 +0200

    added saving token in sessionStorage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,19 +1,21 @@
 import { Button } from "@aws-amplify/ui-react";
 import React from "react";
 import { useCookies } from "react-cookie";
+import { useLogin, useLogout } from "../utils/auth";
 
 function LoginPage() {
-  const [cookies, setCookies, removeCookie] = useCookies(["authToken"]);
+  const [cookies] = useCookies(["authToken"]);
+  const login = useLogin();
 
   const checkToken = () => {
     const token = cookies.authToken;
     if (token) console.log("token exists", token);
     else console.log("token doesn't exist");
   };
-
-  const removeToken = () => {
-    removeCookie("authToken", { path: "/" });
-    console.log("Token został usunięty.");
+  const addToken = () => {
+    const token = "exampleToken";
+    login(token);
+    console.log("Token has been added.");
   };
 
   return (
@@ -21,15 +23,8 @@ function LoginPage() {
       <h1 className=" text-3xl font-bold underline"> Login Page</h1>
       <div>
         <Button onClick={checkToken}>check token</Button>
-        <Button
-          onClick={() => {
-            setCookies("authToken", "TokenValue", { path: "/" });
-          }}
-        >
-          {" "}
-          Dodaj token
-        </Button>
-        <Button onClick={removeToken}> usun token</Button>
+        <Button onClick={addToken}> Dodaj token</Button>
+        <Button onClick={useLogout}> usun token</Button>
       </div>
     </div>
   );
