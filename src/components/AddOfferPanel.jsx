c031094bad0d3532199592c964577d1aae3485d6
ruffaaw <rafal.p.ciupek@gmail.com>
2024-09-18 19:31:49 +0200

    fixed addOfferPanel

--- src/components/AddOfferPanel.jsx
+++ src/components/AddOfferPanel.jsx
@@ -61,7 +61,7 @@ function AddOfferPanel({ onSave, onCancel }) {
   };
 
   return (
-    <div className=" fixed inset-0 bg-light-grey bg-opacity-75 flex item-center justify-center z-50">
+    <div className=" fixed inset-0 bg-light-grey bg-opacity-75 flex items-center justify-center z-50">
       <div className="bg-white p-6 rounded-lg shadow-lg w-1/3">
         <h2 className="text-4xl font-bold mb-4 font-poppins">
           Dodaj nową ofertę
