56187ded729924460077fc9d973a5f44b3370872
ruffaaw <rafal.p.ciupek@gmail.com>
2025-02-09 23:38:41 +0100

    added client standard config

---
+++ src/config/clientStandardConfig.js
@@ -0,0 +1,8 @@
+const clientStandardConfig = [
+  { value: "Do wejścia", label: "Do wejścia", color: "#4CAF50" }, // Zielony - symbolizuje gotowość do wejścia
+  { value: "Do remontu", label: "Do remontu", color: "#F44336" }, // Czerwony - symbolizuje potrzebę remontu
+  { value: "Deweloperski", label: "Deweloperski", color: "#FFC107" }, // Żółty - stan deweloperski
+  { value: "Do odświeżenia", label: "Do odświeżenia", color: "#2196F3" }, // Niebieski - symbolizuje drobne poprawki
+];
+
+export default clientStandardConfig;
