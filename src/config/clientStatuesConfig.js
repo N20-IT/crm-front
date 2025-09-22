dddebd2709d795ebe6ca59e8bd6fb14d41d6977f
Damian Kaniewski <91936398+damiankaniewski@users.noreply.github.com>
2025-09-22 18:22:03 +0200

    Update clientStatuesConfig.js

--- src/config/clientStatuesConfig.js
+++ src/config/clientStatuesConfig.js
@@ -2,7 +2,6 @@ const clientStatuesConfig = [
   { value: "Brak", label: "Brak", color: "black" },
   { value: "Wolny", label: "Wolny", color: "#4CAF50" }, // Zielony - symbolizuje dostępność
   { value: "Zajęty", label: "Zajęty", color: "#F44336" }, // Czerwony - symbolizuje brak dostępności
-  { value: "Do zbrifowania", label: "Do zbrifowania", color: "#FFC107" },
 ];
 
 export default clientStatuesConfig;
